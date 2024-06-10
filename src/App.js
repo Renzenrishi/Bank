a205506d88d03f6918d374262e3017b95cca859e
Renzenrishi <renzenrishi@gmail.com>
2024-06-10 20:08:31 +0000

    m

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import {Switch, Route, Redirect} from 'react-router-dom'
 
 import './App.css'
 
-import Login from './Login'
+import Login from './components/Login'
 
-import Home from './Home'
-import NotFound from './NotFound'
-import ProtectedRoute from './ProtectedRoute'
+import Home from './components/Home'
+import NotFound from './components/NotFound'
+import ProtectedRoute from './components/ProtectedRoute'
 
 const App = () => (
   <Switch>
